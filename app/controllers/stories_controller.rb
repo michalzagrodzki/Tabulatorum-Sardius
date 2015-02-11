d323a8091cd514fabfcadf89fe67bf742e96d597
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-11 21:04:17 +0100

    add controller in :stories for index function

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,10 @@
 class StoriesController < ApplicationController
 
+  # function for listing all stories
+  def index
+    @stories = Story.all
+  end
+
   # function for showing specific story
   def show
     @story = Story.find(params[:id])
