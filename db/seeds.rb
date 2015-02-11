0543df9c2b965557aa556b62a4e5eb09b572ca04
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-11 21:37:16 +0100

    create seed for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encodes files in utf-8
+# ruby encoding: utf-8
+
+# This is examples for stories - for testing purposes
+
+# List of stories for welcome_page
+story_list = [
+    ["Malta", "This is first text about Malta", "link to image",],
+    ["Cinque Terre", "This is second text about Cinque Terre", "link to image",],
+    ["Wicklow Way", "This is third text about Wicklow Way", "link to image",]
+]
+
+#loop for putting story_list into db
+story_list.each do |name, story_part_1, link_to_image|
+  Story.create( name: name, story_part_1: story_part_1, link_to_image: link_to_image )
+end
+
+
+
