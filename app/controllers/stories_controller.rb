7b1771508937e0db01ab2208d08a144a90d497e3
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-11 21:01:11 +0100

    add controller in :stories for show function

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,9 @@
 class StoriesController < ApplicationController
+
+  # function for showing specific story
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  #for this time, other funcions - new, update, delete - are disabled
 end
