08a3e6e8280b3904669641706136c1f4ce078ac5
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-14 22:12:23 +0100

    Add new value, test it, then abort that feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 
 # List of stories for welcome_page
 story_list = [
-    ["Malta", "This is first text about Malta", "link to image",],
-    ["Cinque Terre", "This is second text about Cinque Terre", "link to image",],
-    ["Wicklow Way", "This is third text about Wicklow Way", "link to image",]
+    ["Malta", "This is first text about Malta", "link to image" ],
+    ["Cinque Terre", "This is second text about Cinque Terre", "link to image" ],
+    ["Wicklow Way", "This is third text about Wicklow Way", "link to image" ]
 ]
 
 #loop for putting story_list into db
-story_list.each do |name, story_part_1, link_to_image|
+story_list.each do |name, story_part_1, link_to_image |
   Story.create( name: name, story_part_1: story_part_1, link_image_1: link_to_image )
 end
 
