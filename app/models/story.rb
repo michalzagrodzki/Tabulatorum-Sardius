562df2c6b3437a714935c759b7b2aaa49b6af3be
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-15 22:52:15 +0100

    Add association between stories and tags.

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,4 +3,6 @@ class Story < ActiveRecord::Base
   # check presence of title and minimum length of text
   validates :name, presence: true, length: { minimum: 3 }
 
+  # association with tags
+  belongs_to :tag
 end
