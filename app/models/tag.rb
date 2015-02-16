44dacba33c14187561793d65cb340bc3b00e6c66
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-16 10:45:14 +0100

    Refactor order of code.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,9 @@
 class Tag < ActiveRecord::Base
 
+  # Association with stories
+  has_many :stories
+
   # Check presence of type of tag and minimum length of 2
   validates :type, presence: true, length: { minimum: 2 }
 
-  # Association with stories
-  has_many :stories
 end
