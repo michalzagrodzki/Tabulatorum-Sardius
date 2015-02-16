8b65186a720045a79928def94819bc65f81b44f4
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-16 10:41:19 +0100

    Add validation for Tag in model.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,8 @@
 class Tag < ActiveRecord::Base
 
+  # Check presence of type of tag and minimum length of 2
+  validates :type, presence: true, length: { minimum: 2 }
+
   # Association with stories
   has_many :stories
 end
