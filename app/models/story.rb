1f79837e744e4e5560b4e7917fc72c525a1c712d
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-18 17:47:05 +0100

    Change association between stories and tags.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,13 @@
 class Story < ActiveRecord::Base
 
   # Association with tags
-  belongs_to :tag
+  has_many :tags
 
   # Check presence of title and minimum length of text
   validates :name, presence: true, length: { minimum: 3 }
 
+  # Allow tags to be destroyed by nested attributes
+  accepts_nested_attributes_for :tags, reject_if: proc { |attrs| attr.all? {
+                                                        |value, key| value.blank? }
+                                                        }
 end
