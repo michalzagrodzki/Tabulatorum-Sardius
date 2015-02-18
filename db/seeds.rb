e069b93af5c67229cd25977fcd8f865e645d50bb
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-18 19:10:36 +0100

    Update seeds.rb with additional data to tags table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,24 @@ story_list = [
     ["Wicklow Way", "This is third text about Wicklow Way", "link to image" ]
 ]
 
-#loop for putting story_list into db
+# loop for putting story_list into db
 story_list.each do |name, story_part_1, link_to_image |
   Story.create( name: name, story_part_1: story_part_1, link_image_1: link_to_image )
+end
+
+tag_list = [
+    ["Hike", 1],
+    ["City", 2],
+    ["Museum", 3],
+    ["Trail", 4],
+    ["Eat", 5],
+    ["Relax", 6],
+
+]
+
+# loop for putting tag_list into db
+tag_list.each do |name, story_id|
+  Tag.create(name: name, story_id: story_id)
 end
 
 
