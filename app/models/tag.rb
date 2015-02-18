e0668ce78e70fdd0d55d993c8858ccbd00fc3eb8
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-18 17:47:55 +0100

    Change association between tags and stories.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
 
   # Association with stories
-  has_many :stories
+  belongs_to :stories
 
   # Check presence of type of tag and minimum length of 2
   validates :type, presence: true, length: { minimum: 2 }
