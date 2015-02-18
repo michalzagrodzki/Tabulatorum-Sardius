25246cc94fb993b76db6577dac25a9727879465d
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-18 17:54:12 +0100

    Add controller for tags, show only specific tag and list all tags.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,40 @@
 class TagsController < ApplicationController
+
+  # restricting elementes which will be fetched:
+  before_action :fetch_element, only: [:show, :index]
+
+  # function for listing all tags
+  def index
+    @tags = Tag.all
+  end
+
+  # function for listing specific tag by it's param
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  # for this time, other functions - new, update, delete - are not defined
+  # unused function
+  def new
+  end
+
+  # unused function
+  def edit
+  end
+
+  # unused function
+  def update
+  end
+
+  # unused function
+  def destroy
+  end
+
+  # private method for fetching elements from model
+  private
+
+  def fetch_element
+    @tags = Tag.all
+  end
+
 end
