4399e082d2cc859f407791e037da7ed204a2e0c4
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-20 19:30:48 +0100

    Create controller for creating and validating new message.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,23 @@
 class ContactsController < ApplicationController
+
+  # Function for creating new contact mail
+  def new
+    @contact = Contact.new
+  end
+
+  # Function for validating email
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+
+    # Message of success or failure
+    if @contact.deliver
+      flash.now[:error] = nil
+      flash.now[:notice] = 'Thank you for contacting with us.'
+    else
+      flash.nowp[:error] = 'Cannot send message, try again.'
+      render :new
+    end
+
+  end
 end
