35228d1d0bb7444b6d9c7a2f364936df1f11686c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-24 20:41:22 +0100

    Setup variables for sending emails in development environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,19 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Setting host for sending emails
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+
+  # Configuring SMTP settings for sending emails
+  config.action_mailer.smtp_settings = {
+      address:                'smtp.gmail.com',
+      port:                   587,
+      domain:                 ENV['GMAIL_DOMAIN'],
+      authentication:         'plain',
+      enable_starttls_auto:   true,
+      user_name:              ENV['GMAIL_USERNAME'],
+      password:               ENV['GMAIL_PASSWORD']
+  }
+
 end
