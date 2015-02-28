b7d1b0221bcf1da013100e4e8aee0b301f29d698
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-28 21:15:43 +0100

    Add template data to seed.db file to show elements on google maps.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 
 # List of stories for welcome_page
 story_list = [
-    ["Malta", "This is first text about Malta", "link to image" ],
-    ["Cinque Terre", "This is second text about Cinque Terre", "link to image" ],
-    ["Wicklow Way", "This is third text about Wicklow Way", "link to image" ]
+    ["Malta", "This is first text about Malta", "link to image", 35.882056, 14.447462 ],
+    ["Cinque Terre", "This is second text about Cinque Terre", "link to image", 44.127062, 9.707102 ],
+    ["Wicklow Way", "This is third text about Wicklow Way", "link to image", 53.083510, -6.256211 ]
 ]
 
 # loop for putting story_list into db
-story_list.each do |name, story_part_1, link_to_image |
-  Story.create( name: name, story_part_1: story_part_1, link_image_1: link_to_image )
+story_list.each do |name, story_part_1, link_to_image, lat, lng |
+  Story.create( name: name, story_part_1: story_part_1, link_image_1: link_to_image, latitude: lat, longitude: lng )
 end
 
 tag_list = [
@@ -27,7 +27,7 @@ tag_list = [
 
 # loop for putting tag_list into db
 tag_list.each do |name, story_id|
-  Tag.create(name: name, story_id: story_id)
+  Tag.create( name: name, story_id: story_id )
 end
 
 
