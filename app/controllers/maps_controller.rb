088377695ba935e78ee5f6d732a14a0072a15667
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-02-28 20:46:47 +0100

    Add function for listing stories on google maps in controller file.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,27 @@
 class MapsController < ApplicationController
+
+  #fetch only index
+  before_action :fetch_elements, only: [ :index ]
+
+  def index
+    @stories = Story.all
+    # variables passed to Google Maps
+    @hash = Gmaps4rails.build_markers(@stories) do |story, marker|
+      marker.lat story.latitude
+      marker.lng story.longitude
+      marker.infowindow story.name
+      marker.json({
+                      # name of story for marker/infowindow
+                      title: story.name,
+                      # value for showing in infowindow
+                      lat: story.latitude
+                  })
+    end
+  end
+
+  private
+
+  def fetch_elements
+    @story = Story.all
+  end
 end
