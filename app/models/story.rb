2f01f13b3489553fe514dc1074cea165f3d94005
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-03 21:31:00 +0100

    Add function search to Story Model, update method validates.

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,7 +4,14 @@ class Story < ActiveRecord::Base
   has_many :tags
 
   # Check presence of title and minimum length of text
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+
+  # Returns story, which contains at least one word from query
+  # Word 'search' is linked to Controller and View
+  def self.search(query)
+    # parameters for exact search
+    where("name like ?", "%#{query}%")
+  end
 
   # Allow tags to be destroyed by nested attributes
   accepts_nested_attributes_for :tags, reject_if: proc { |attrs| attr.all? {
