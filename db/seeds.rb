f43d5bfc467328d117f232a4a07daf6a16d1d601
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-06 21:15:58 +0100

    New template data in seeds.rb following new columns.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 
 # List of stories for welcome_page
 story_list = [
-    ["Malta", "This is first text about Malta", "link to image", 35.882056, 14.447462 ],
-    ["Cinque Terre", "This is second text about Cinque Terre", "link to image", 44.127062, 9.707102 ],
-    ["Wicklow Way", "This is third text about Wicklow Way", "link to image", 53.083510, -6.256211 ]
+    ["Malta", "This is first text about Malta", "This is second text about Malta", "This is third text about Malta", "This is advice text for Malta", "link to image", 35.882056, 14.447462 ],
+    ["Cinque Terre", "This is first text about Cinque Terre", "This is second text about Cinque Terre", "This is advice text for Cinque Terre", "link to image", 44.127062, 9.707102 ],
+    ["Wicklow Way", "This is first text about Wicklow Way", "This is advice text for Wicklow Way", "link to image", 53.083510, -6.256211 ]
 ]
 
 # loop for putting story_list into db
-story_list.each do |name, story_part_1, link_to_image, lat, lng |
-  Story.create( name: name, story_part_1: story_part_1, link_image_1: link_to_image, latitude: lat, longitude: lng )
+story_list.each do |name, story_part_1, story_part_2, story_part_3, advice_text, link_to_image, lat, lng |
+  Story.create( name: name, story_part_1: story_part_1, story_part_2: story_part_2, story_part_3: story_part_3, advice_text: advice_text, link_image_1: link_to_image, latitude: lat, longitude: lng )
 end
 
 tag_list = [
