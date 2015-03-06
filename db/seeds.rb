ead391de9180de62cdfb80fbdc2aa70eafb96031
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-06 23:37:25 +0100

    Comment out loop for setting Story table. Replace it with manual Class creation. Update data for testing purpose. Add comments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,55 @@
 
 # This is examples for stories - for testing purposes
 
+######
+
+# This list is temporarely commented out
+
 # List of stories for welcome_page
-story_list = [
-    ["Malta", "This is first text about Malta", "This is second text about Malta", "This is third text about Malta", "This is advice text for Malta", "link to image", 35.882056, 14.447462 ],
-    ["Cinque Terre", "This is first text about Cinque Terre", "This is second text about Cinque Terre", "This is advice text for Cinque Terre", "link to image", 44.127062, 9.707102 ],
-    ["Wicklow Way", "This is first text about Wicklow Way", "This is advice text for Wicklow Way", "link to image", 53.083510, -6.256211 ]
-]
+# story_list = [
+#    ["Malta", "This is first text about Malta", "This is second text about Malta", "This is third text about Malta", "This is advice text for Malta", "link to image", 35.882056, 14.447462 ],
+#    ["Cinque Terre", "This is first text about Cinque Terre", "This is second text about Cinque Terre", "This is advice text for Cinque Terre", "link to image", 44.127062, 9.707102 ],
+#    ["Wicklow Way", "This is first text about Wicklow Way", "This is advice text for Wicklow Way", "link to image", 53.083510, -6.256211 ]
+# ]
 
 # loop for putting story_list into db
-story_list.each do |name, story_part_1, story_part_2, story_part_3, advice_text, link_to_image, lat, lng |
-  Story.create( name: name, story_part_1: story_part_1, story_part_2: story_part_2, story_part_3: story_part_3, advice_text: advice_text, link_image_1: link_to_image, latitude: lat, longitude: lng )
-end
+# story_list.each do |name, story_part_1, story_part_2, story_part_3, advice_text, link_to_image, lat, lng |
+  # Story.create( name: name, story_part_1: story_part_1, story_part_2: story_part_2, story_part_3: story_part_3, advice_text: advice_text, link_image_1: link_to_image, latitude: lat, longitude: lng )
+# end
+
+# End of temporarely commented out list
+
+######
+
+# This is examples for stories - for testing purposes
+
+Story.create( name: 'Malta',
+              story_part_1: 'This is first text about Malta',
+              story_part_2: 'This is second text about Malta',
+              story_part_3: 'This is third text about Malta',
+              advice: 'This is advice text for Malta',
+              link_image_1: '',
+              latitude: 35.882056,
+              longitude: 14.447462 )
+
+Story.create( name: 'Cinque Terre',
+              story_part_1: 'This is first text about Cinque Terre',
+              story_part_2: 'This is second text about Cinque Terre',
+              story_part_3: '',
+              advice: 'This is advice text for Cinque Terre',
+              link_image_1: '',
+              latitude: 44.127062,
+              longitude: 9.707102 )
+
+Story.create( name: 'Wicklow Way',
+              story_part_1: 'This is first text about Wicklow Way',
+              story_part_2: '',
+              story_part_3: '',
+              advice: 'This is advice text for Wicklow Way',
+              link_image_1: '',
+              latitude: 53.083510,
+              longitude: -6.256211 )
+
 
 tag_list = [
     ["Hike", 1],
@@ -29,6 +67,8 @@ tag_list = [
 tag_list.each do |name, story_id|
   Tag.create( name: name, story_id: story_id )
 end
+
+# End of examples for stories
 
 
 
