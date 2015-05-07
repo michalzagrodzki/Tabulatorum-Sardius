df30dad38cca04a33ed5cc13939608a5df057359
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-07 16:44:05 +0200

    Add helper for wrapping links into images.

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,12 @@
 module StoriesHelper
+
+  # Link wrapped in image
+  def link_in_image(image_path, target_link, options_image={}, options_link={})
+    link_to(image_tag(image_path, options_image), target_link, options_link)
+  end
+
+  def unused
+  image_tag(picture.link, class: 'img-responsive img-thumbnail', alt:"")
+  end
+
 end
