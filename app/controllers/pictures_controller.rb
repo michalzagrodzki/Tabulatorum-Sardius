4367eea0d9ad84a61b2145ca6f5e8dd7e8d495b9
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-07 18:47:50 +0200

    Add strong parameters in Pictures Controller.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,18 @@ class PicturesController < ApplicationController
   expose(:picture)
   expose(:story)
 
+  # restricing elements, which can be accessed
+  before_action :fetch_picture, only: [ :index ]
+
+
   def index
+  end
+
+  # private method for fetching elements to show
+  private
+
+  def fetch_picture
+    pictures = Picture.all
   end
 
 end
