1d34bc8128b9168cdc6d6465f70c8da500f4e86d
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-11 15:23:54 +0200

    Add function for adding new Chapter - for now just text.

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,5 +1,8 @@
 class ChaptersController < ApplicationController
 
+  expose(:chapter)
+  expose(:story)
+
   # set rights for guest and user
   before_action :authenticate_user!, except: [ :index ]
 
@@ -7,4 +10,22 @@ class ChaptersController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+    self.chapter = Chapter.new(chapter_params)
+
+    if chapter.save
+      story.chapters << chapter
+      redirect_to story, notice: 'Chapter added.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def chapter_params
+    params.require(:chapter).permit(:id, :text)
+  end
 end
