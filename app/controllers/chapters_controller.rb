8f5b609cb1646ccb1fd5ee97ce12a6e542eac6f7
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-12 19:43:26 +0200

    Clean Chapter Controller - remove unnecessary functions.

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -8,13 +8,6 @@ class ChaptersController < ApplicationController
   # set rights for guest and user
   before_action :authenticate_user!, except: [ :index ]
 
-  # temporarily put index - test later if it is necessary
-  def index
-  end
-
-  def new
-  end
-
   def create
     self.chapter = Chapter.new(chapter_params)
 
