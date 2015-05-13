7ca630f182b6b53ad67e97b6cbc0ef3bdf9edc84
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-13 15:36:05 +0200

    Add route for method edit_pictures.

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
 
   resources :pictures
 
+  # Path to edit_pictures method in StoriesController (Edit and Update)
+  get 'stories/:id/edit_pictures', to: 'stories#edit_pictures', as: 'edit_pictures'
+  patch 'stories/:id/edit_pictures', to: 'stories#update_pictures', as: 'update_pictures'
+
   # Link to contact page
   match '/contact_us', to: 'contacts#new', via: 'get', :as => :contact
   resources "contacts", only: [:new, :create]
