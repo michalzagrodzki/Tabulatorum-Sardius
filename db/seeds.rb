7f799fcec69d9fd4b7ea4907f7076402899f1b48
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-22 11:31:05 +0200

    Modify seeds with example data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,16 @@ Picture.delete_all
 
 ### List of stories - shows layout of text
 
-part_1_story = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way."
+part_1_story = "A lovely walk around a lake."
 
-part_2_story = "When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word and and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their projects again and again. And if she hasn’t been rewritten, then they are still using her."
 
-part_3_story = "Not so far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way."
-
-advice_text = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. What's happened to me?  he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame."
+advice_text = "Best way to reach is by train. Jump out at Hvalstad or Vakås and head west."
 
 ### List of image links
 
-link_to_main_page_image = 'http://s6.postimg.org/3x9u55ym9/story_main_page_image.jpg'
+link_to_main_page_image = 'http://s6.postimg.org/iglat1q81/main_page.jpg'
 
-link_to_head_image = 'http://s6.postimg.org/hs84nmt1d/story_header_image.jpg'
+link_to_head_image = 'http://s6.postimg.org/3yji7b9j5/header.jpg'
 
 ### Database entries
 
@@ -36,23 +33,11 @@ link_to_head_image = 'http://s6.postimg.org/hs84nmt1d/story_header_image.jpg'
 
 #####
 
-Story.create( name: 'Malta',
+Story.create( name: 'Afternoon walk',
               main_page_image: link_to_main_page_image,
               header_image: link_to_head_image,
-              latitude: 35.882056,
-              longitude: 14.447462 )
-
-Story.create( name: 'Cinque Terre',
-              main_page_image: link_to_main_page_image,
-              header_image: link_to_head_image,
-              latitude: 44.127062,
-              longitude: 9.707102 )
-
-Story.create( name: 'Wicklow Way',
-              main_page_image: link_to_main_page_image,
-              header_image: link_to_head_image,
-              latitude: 53.083510,
-              longitude: -6.256211 )
+              latitude: 59.859748,
+              longitude: 10.422832 )
 
 #####
 
@@ -64,14 +49,6 @@ Story.create( name: 'Wicklow Way',
 
 Chapter.create(
     text: part_1_story,
-    story_id: 1 )
-
-Chapter.create(
-    text: part_2_story,
-    story_id: 1 )
-
-Chapter.create(
-    text: part_3_story,
     story_id: 1 )
 
 #####
@@ -92,136 +69,133 @@ Advice.create(
 
 #####
 
-link_to_regular_image_01 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_02 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_03 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_04 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_05 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_06 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_07 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_08 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
-link_to_regular_image_09 = 'http://s6.postimg.org/72puhmrup/story_normal_image.jpg'
+link_to_regular_image_01 = 'http://s6.postimg.org/9qjmegjcx/IMG_3548.jpg'
+link_to_regular_image_02 = 'http://s6.postimg.org/uyxavvxtt/IMG_3552.jpg'
+link_to_regular_image_03 = 'http://s6.postimg.org/3vzmkh5vl/IMG_3574.jpg'
+link_to_regular_image_04 = 'http://s6.postimg.org/523mj9u69/IMG_3577.jpg'
+link_to_regular_image_05 = 'http://s6.postimg.org/5vg8bw075/IMG_3590.jpg'
+link_to_regular_image_06 = 'http://s6.postimg.org/d17zec9a9/IMG_3599.jpg'
+link_to_regular_image_07 = 'http://s6.postimg.org/81aj6e3nl/IMG_3619.jpg'
+link_to_regular_image_08 = 'http://s6.postimg.org/ap4kuhm2p/IMG_3645.jpg'
+link_to_regular_image_09 = 'http://s6.postimg.org/v5azyz6yp/IMG_3659.jpg'
 
 # Malta Images
 
 # Regular Images
 
 picture_01 = Picture.create(
-    title: 'Regular image 01',
-    description: 'Spectacular view.',
+    title: 'Houses in autumn colors.',
+    description: 'Fabulous view.',
     link: link_to_regular_image_01,
-    location: 'Sliema, Malta',
-    latitude: 35.897622,
-    longitude: 14.538297,
+    location: 'Semsvannet, Asker',
+    latitude: 59.860157,
+    longitude: 10.456850,
     story_id: 1,
-    chapter_id: 1,
-    advice_id: 1
+    chapter_id: 1
+
 )
 
 picture_01.save!
 
 picture_02 = Picture.create(
-    title: 'Regular image 02',
-    description: 'Spectacular view.',
+    title: 'Path sign.',
+    description: 'Road leading to lake.',
     link: link_to_regular_image_02,
-    location: 'Sliema, Malta',
-    latitude: 35.897222,
-    longitude: 14.505297,
+    location: 'Semsvannet, Asker',
+    latitude: 59.859717,
+    longitude: 10.455523,
     story_id: 1,
-    chapter_id: 1,
-    advice_id: 1
+    chapter_id: 1
+
 )
 
 picture_02.save!
 
 picture_03 = Picture.create(
-    title: 'Regular image 03',
-    description: 'Spectacular view.',
+    title: 'View over lake towards west.',
+    description: 'Splendid view.',
     link: link_to_regular_image_03,
-    location: 'Sliema, Malta',
-    latitude: 35.894322,
-    longitude: 14.508397,
+    location: 'Semsvannet, Asker',
+    latitude: 59.858550,
+    longitude: 10.434441,
     story_id: 1,
-    chapter_id: 1,
-    advice_id: 1
+    chapter_id: 1
+
 )
 
 picture_03.save!
 
 picture_04 = Picture.create(
-    title: 'Regular image 04',
-    description: 'Spectacular view.',
+    title: 'Entrance to kindergarten.',
+    description: 'Place where kids can play.',
     link: link_to_regular_image_04,
-    location: 'Sliema, Malta',
-    latitude: 35.892322,
-    longitude: 14.501297,
-    story_id: 1,
-    chapter_id: 2
+    location: 'Semsvannet, Asker',
+    latitude: 59.859001,
+    longitude: 10.432429,
+    story_id: 1
 )
 
 picture_04.save!
 
 picture_05 = Picture.create(
-    title: 'Regular image 05',
-    description: 'Spectacular view.',
+    title: 'Autumn tree.',
+    description: 'Single tree over lake.',
     link: link_to_regular_image_05,
-    location: 'Sliema, Malta',
-    latitude: 35.899322,
-    longitude: 14.505297,
-    story_id: 1,
-    chapter_id: 2
+    location: 'Semsvannet, Asker',
+    latitude: 59.862221,
+    longitude: 10.429641,
+    story_id: 1
 )
 
 picture_05.save!
 
 picture_06 = Picture.create(
-    title: 'Regular image 06',
-    description: 'Spectacular view.',
+    title: 'Autumn grass.',
+    description: 'Marvelous view.',
     link: link_to_regular_image_06,
-    location: 'Sliema, Malta',
-    latitude: 35.891422,
-    longitude: 14.503297,
-    story_id: 1,
-    chapter_id: 2
+    location: 'Semsvannet, Asker',
+    latitude: 59.864039,
+    longitude: 10.424582,
+    story_id: 1
 )
 
 picture_06.save!
 
 picture_07 = Picture.create(
-    title: 'Regular image 07',
-    description: 'Spectacular view.',
+    title: 'Horse.',
+    description: 'Horse enjoys weather.',
     link: link_to_regular_image_07,
-    location: 'Sliema, Malta',
-    latitude: 35.895422,
-    longitude: 14.507597,
+    location: 'Semsvannet, Asker',
+    latitude: 59.860198,
+    longitude: 10.415467,
     story_id: 1,
-    chapter_id: 3
+    advice_id: 1
 )
 
 picture_07.save!
 
 picture_08 = Picture.create(
-    title: 'Regular image 08',
-    description: 'Spectacular view.',
+    title: 'View over lake towards north.',
+    description: 'Charming view.',
     link: link_to_regular_image_08,
-    location: 'Sliema, Malta',
-    latitude: 35.899222,
-    longitude: 14.501897,
+    location: 'Semsvannet, Asker',
+    latitude: 59.856720,
+    longitude: 10.408549,
     story_id: 1,
-    chapter_id: 3
+    advice_id: 1
 )
 
 picture_08.save!
 
 picture_09 = Picture.create(
-    title: 'Regular image 09',
-    description: 'Spectacular view.',
+    title: 'Autumn afternoon.',
+    description: 'Pleasing view.',
     link: link_to_regular_image_09,
-    location: 'Sliema, Malta',
-    latitude: 35.892122,
-    longitude: 14.501697,
+    location: 'Semsvannet, Asker',
+    latitude: 59.854532,
+    longitude: 10.413417,
     story_id: 1,
-    chapter_id: 3
+    advice_id: 1
 )
 
 picture_09.save!
@@ -236,22 +210,6 @@ User.create(
     email: "ok@ok.ok",
     password: 'heyhoheyho',
     password_confirmation: 'heyhoheyho' )
-
-
-#
-# tag_list = [
-#    ['Hike', 1],
-#    ['City', 2],
-#    ['Museum', 3],
-#    ['Trail', 4],
-#    ['Eat', 5],
-#    ['Relax', 6]
-#]
-
-# loop for putting tag_list into db
-#tag_list.each do |name, story_id|
-#  Tag.create( name: name, story_id: story_id )
-#end
 
 # End of examples for stories
 
