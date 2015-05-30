6e617046e53d3bc7b53d676a4a6ef432547b3d07
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-30 23:30:12 +0200

    Add factory for stories controller.

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -1,6 +1,10 @@
+require 'faker'
+
 FactoryGirl.define do
-  factory :story do
-    name "MyString"
+  factory :story do |f|
+    f.name { Faker::Address.city }
+    f.latitude { Faker::Address.latitude }
+    f.longitude { Faker::Address.longitude }
   end
 
 end
