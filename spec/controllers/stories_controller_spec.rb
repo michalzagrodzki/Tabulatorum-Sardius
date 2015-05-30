99fc443bd580091b54d4dd9b8bc75d593cd7e91b
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-05-30 23:29:58 +0200

    Add test for Stories Controller

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe StoriesController, type: :controller do
+describe StoriesController do
+
+  describe "GET #index" do
+    it "populates an array of stories" do
+      story = Factory(:story)
+      get :index
+      assigns(:stories).should eq([story])
+    end
+
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
 
 end
